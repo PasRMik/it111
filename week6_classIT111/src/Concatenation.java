b6a944e3c91856287b6ccc9faf38a0e83782ce9a
PasRMik <166089442+PasRMik@users.noreply.github.com>
2024-06-04 10:49:00 -0700

    Add files via upload

---
+++ week6_classIT111/src/Concatenation.java
@@ -0,0 +1,9 @@
+package src.src;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String color = "red";
+        int money = 100;
+        System.out.println(color + money);
+    }
+}
--- week6_classIT111/src/ShowWindow2.java
+++ week6_classIT111/src/ShowWindow2.java
@@ -1,28 +1,17 @@
 package src.src;
-import javax.swing. *;
-
+import javax.swing.*;
 import java.awt.*;
-
-import static java.awt.SystemColor.text;
 
 public class ShowWindow2 {
 
     public static void main(String[] args) {
         // declare our variables
         // we are going to make them CONSTANTS
-
-
-
         final int WINDOW_WIDTH = 350;
         final int WINDOW_HEIGHT = 400;
 
         // Create a window
-
         JFrame window = new JFrame();
-        JLabel label1 = new JLabel("My First Big Label");
-
-        JTextField myTextField1 = new JTextField();
-
 
         // set the title
         window.setTitle("A Simple Window");
@@ -31,28 +20,38 @@ public class ShowWindow2 {
         window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
         // close the window and exit the program
-
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
 
+        // Get the content pane and set layout and background color
+        Container container = window.getContentPane();
+        container.setLayout(null);
+        container.setBackground(Color.RED);
 
+        // Create and add the first label
+        JLabel label1 = new JLabel("My First Big Label!!!");
+        label1.setBounds(20, 20, 300, 50);
+        label1.setFont(new Font("Verdana", Font.PLAIN, 18)); // Not bold
+        container.add(label1);
 
+        // Create and add the first text field
+        JTextField myTextField1 = new JTextField();
+        myTextField1.setBounds(20, 80, 300, 50);
+        container.add(myTextField1);
+
+        // Create and add the second label
+        JLabel label2 = new JLabel("My Second Big Label!");
+        label2.setBounds(20, 150, 300, 50);
+        label2.setFont(new Font("Verdana", Font.BOLD, 18)); // Bold
+        container.add(label2);
+
+        // Create and add the second text field
+        JTextField myTextField2 = new JTextField();
+        myTextField2.setBounds(20, 210, 300, 50);
+        container.add(myTextField2);
 
         // display the window - if we do not display the window, it will not display!
         // displaying the window is a boolean
         window.setVisible(true);
-
-
-        window.getContentPane().setBackground(Color.RED);
-        window.setLocation(500, 300);
-        window.setBounds(20, 50, 220, 30);
-        window.setFont(new Font("Verdana", Font.PLAIN, 20));
-        myTextField1.setBounds(20, 80, 300, 50);
-
     }
-
-
-    }
-
-
-
-
+}
